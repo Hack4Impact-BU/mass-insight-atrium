c72a779f3666b0ff869055f1082f4eea0e7b33f3
grokzero0 <grok1618@gmail.com>
2025-03-06 00:39:30 -0500

    create basic navbar

--- app/(data-entry-pages)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { FileProvider } from "@/utils/upload-data/file-context";
-
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <FileProvider>{children}</FileProvider>;
-}
